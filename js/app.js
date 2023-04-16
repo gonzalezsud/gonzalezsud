d86ffd2ce2655c1a3ffb9b194110d0b8638194bc
Noe Gonzalez <gonzalezsud@yahoo.com>
2023-04-15 19:46:49 -0600

    2

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import data from "./data.js"
+
+const url = "./stays.json";
+
+const datos = data.getData();
+
+console.log(datos);
---
+++ js/dom.js
@@ -0,0 +1,5 @@
+/* 
+Aqui van todas las funciones o variables relacionadas 
+con la manipulación del DOM en la aplicación
+*/
+
