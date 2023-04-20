10927e213ad05021f0396990e9857d31647ae414
Noe Gonzalez <gonzalezsud@yahoo.com>
2023-04-20 10:30:15 -0600

    2143

--- js/dom.js
+++ js/dom.js
@@ -28,12 +28,12 @@ const newcard = (obj) => {
 
     div.innerHTML = `
     
-    <div class="img card-img-modified">
-      <img class="w-100 h-100 rounded-4"src="${obj.photo}" alt="${obj.city}">
+    <div class="img col card card-img-modified">
+      <img class="w-100 h-100 col card-img-top rounded-4" src="${obj.photo}" alt="${obj.city}">
       </div>
-      <div class="info d-flex justify-content-between mt-3">
-      <p class="d-flex flex-column">
-      <span class="">${obj.superHost[true]}</span>
+      <div class="info card-body d-flex justify-content-between mt-3">
+      <p class="d-flex card-text flex-column">
+      <span class="fw-bold">${obj.superHost[true]}</span>
       <span>${obj.type}</span>
       <span class="material-symbols-outlined">star</span>
       <span>${obj.rating}</span>
